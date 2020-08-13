f3afad11f46abd808bb27efa32ffebc0d892d385
Amanda McCabegit config --global user.email amccabe1316@gmail.com <amccabe1316@gmail.com>
2020-08-13 07:59:33 -0600

    updates

---
+++ main.js
@@ -0,0 +1,38 @@
+'use strict';
+
+function generateQuesion() {
+    let question = [];
+    for (let i = 0; i < QUESTIONS.length; i++) {
+        const checkedClass = QUESTIONS[i].completed ?
+    }
+}
+
+function generateStartPage() {
+    return ` <div class="startPage">
+            <h3> Do you know Percy Jackson?</h3>
+            <p>Take this quiz to find out.</p>
+            <button class="startQuiz">Start Quiz</button>
+        </div>
+        `;
+}
+
+function generateEndPage() {
+
+}
+
+function main() {
+    let startPage = generateStartPage();
+    $('main').html(startPage);
+}
+
+function () {
+
+}
+
+function () {
+
+}
+
+$('main').on('click', ''.startQuiz ',function(){
+
+});
