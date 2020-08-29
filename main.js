45bdc18d5db5a6536f63956e10679c48d2e33917
Amanda McCabe <amccabe1316@gmail.com>
2020-08-29 15:47:25 -0600

    updates

--- main.js
+++ main.js
@@ -64,18 +64,17 @@ function generateStartPage() {
   return ` <div class="startPage">
             <h3> Do you know Percy Jackson?</h3>
             <p>Take this quiz to find out.</p>
-            <button class="startQuiz" type="submit" value="submit">Start Quiz</button>
+            <button id="startQuiz" type="submit" value="submit">Start Quiz</button>
         </div>
         `;
 }
 
-
 function generateQuestionPage(){
-  const i = quiz.currentQuestion;
+  //const i = quiz.currentQuestion;
   return `
       <div class="group">
         <p class="large-text">question <span class="text-yellow">${quiz.currentQuestion +1} of 5</span></p>
-        <p>${quiz.questions[i].question}</p>
+         <p>${quiz.questions[0].question}'</p>
       </div>
       <div>
         <form action="/action_page.php" method="get">
@@ -99,8 +98,8 @@ function generateQuestionPage(){
           </div>
   
           <div class="button-group">
-            <button id="submit-button" class="button" type="submit" value="submit">submit.</button>
-            <button id="restart-button" class="button" type="submit" value="restart">restart.</button>
+            <button id="submit-button" type="submit" value="submit">submit.</button>
+            <button id="restart-button" type="submit" value="restart">restart.</button>
           </div>
         </form>
       </div>
@@ -110,65 +109,98 @@ function generateQuestionPage(){
 }
 
 
-// function generateEndPage() {
+function generateResultsPage(){
+  return `
+          <div class="group">
+            <p class="large-text">results</p>
+            <p><span class="correct-text">correct answer:</span> A pen</p>
+          <div class="group">
+            <p class="correct-text">correct <span class="text-yellow">1 / 6</span></p>
+          </div>
+          <div class="group">
+            <div class="explanation" id="explanation-box"> 
+              <p class="explanation-text">The first thing Chiron gives Percy is a pen. A pen that once clicked turns into a sword.</p>
+            </div>
+          </div>
+          <div class="group">
+            <form action="/action_page.php" method="get">
+              <div class="button-group">
+                <button id="continue-btn" class="button" type="submit" value="continue">Continue</button>
+              </div>
+            </form>
+          </div>`;
+}
 
-// }
 
 function renderStartPage() {
   $('main').html(generateStartPage());
+}
+
+function handleStartButton() {
+  $('main').on('click', '#startQuiz', function(event){
+    event.preventDefault();
+    renderQuestions();
+  });
 }
 
 function renderQuestions(){
   $('main').html(generateQuestionPage());
 }
 
-function handleStartButton() {
-  $('main').on('click', '.startQuiz', function(event){
+function handleSubmitButton(){
+  $('main').on('click', '#submit-button', function(event){
     event.preventDefault();
-    renderQuestions();
+    //gather the input
+    //const selected = $('input:checked');
+    //const answer = selected.val();
+    //results = checkAnswer(answer);
+    //render result or correct answer page
+    renderResultsPage(); //build
   });
 }
 
-// function handleSubmitButton(){
-//   $('main').on('click', '#submitButton', function(event){
+function handleRestartButton(){
+  $('main').on('click', '#restart-button', event => {
+    event.preventDefault();
+    //score = 0;
+    //QUESTIONS.currentQuestion = 0;
+    renderStartPage();
+  });
+}
+
+function renderResultsPage(){
+  $('main').html(generateResultsPage());
+}
+
+// function handleContinueButton() {
+//   $('main').on('click', '#continue-btn', function(event){
 //     event.preventDefault();
-//     //gather the input
-//     const selected = $('input:checked');
-//     const answer = selected.val();
-//     results = checkAnswer(answer);
-//     render result or correct answer page
-//     renderAnswerPage(results); //build
+//     renderQuestions();
 //   });
 // }
 
-// function handleRestartButton(){
-//   $('main').on('click', '#restart-button', event => {
-//     event.preventDefault();
-//     //score = 0;
-//     QUESTIONS.currentQuestion = 0;
-//     //renderQuestionPage();
-//   });
-// }
 
 // function checkAnswer(ans){
 //   const i = QUESTIONS.currentQuestion;
 //   if(ans ===  QUESTIONS.questions[i].correctAnswer){
 //     return 'You got it right!', score++;
-    
 //   }
 //   else{
 //     return 'You got it wrong, sorry.';
-//   }
-    
-    
-  
+//   } 
 // }
+
+// function generateEndPage() {
+
+// }
+
+
 
 function quizApp(){
   renderStartPage();
   handleStartButton();
-  //handleSubmitButton();
-  //handleRestartButton();  
+  handleSubmitButton();
+  handleRestartButton(); 
 }
 
 $(quizApp);
